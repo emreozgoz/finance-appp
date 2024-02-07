d06e43344f3b80d9271a57e1705b298f8c37be7b
Emre ÖZGÖZ <emreozgoz@gmail.com>
2024-02-07 16:30:31 +0300

    feat: axios request added

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,16 +1,20 @@
-import React, { useState } from "react";
+import React, { SyntheticEvent, useState } from "react";
 
 type Props = {};
 
 const Search: React.FC<Props> = (props: Props): JSX.Element => {
   const [search, setSearch] = useState<string>("");
-  const onClick = (e: any) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
+    console.log(e);
+  };
+  const onClick = (e: SyntheticEvent) => {
     console.log(e);
   };
   return (
     <div>
-      <input value={search} onChange={(e) => onClick(e)}></input>
+      <input value={search} onChange={(e) => handleChange(e)}></input>
+      <button onClick={(e) => onClick(e)}></button>
     </div>
   );
 };
