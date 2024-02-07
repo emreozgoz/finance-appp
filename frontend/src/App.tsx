28e46fcfa5943400be83a5ff4136538e13f8882a
Emre ÖZGÖZ <emreozgoz@gmail.com>
2024-02-07 13:22:17 +0300

    feat: add search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Card from "./Components/Card/Card";
-import "./Components/Card/Card.css"
+import "./Components/Card/Card.css";
 import CardList from "./Components/CardList/CardList";
+import Search from "./Components/Search/Search";
 
 function App() {
   return (
     <div className="App">
+      <Search />
       <CardList />
     </div>
   );
--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -3,14 +3,14 @@ import Card from "../Card/Card";
 
 interface Props {}
 
-const CardList = (props: Props) => {
+const CardList: React.FC<Props> = (props: Props): JSX.Element => {
   return (
     <div>
-      <Card companyName="Apple" ticker="asd" price={120}  />
-      <Card companyName="Apple1" ticker="asd1" price={121}  />
-      <Card companyName="Apple2" ticker="asd2" price={122}  />
-      <Card companyName="Apple3" ticker="asd3" price={123}  />
-      <Card companyName="Apple4" ticker="asd4" price={124}  />
+      <Card companyName="Apple" ticker="asd" price={120} />
+      <Card companyName="Apple1" ticker="asd1" price={121} />
+      <Card companyName="Apple2" ticker="asd2" price={122} />
+      <Card companyName="Apple3" ticker="asd3" price={123} />
+      <Card companyName="Apple4" ticker="asd4" price={124} />
     </div>
   );
 };
