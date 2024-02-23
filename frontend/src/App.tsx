0dd49c043a9d120a258a77cadbab649cfaa6a44f
Emre ÖZGÖZ <emreozgoz@gmail.com>
2024-02-23 15:10:02 +0300

    feat: card portfolio and already exist condition

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,9 +6,11 @@ import CardList from "./Components/CardList/CardList";
 import Search from "./Components/Search/Search";
 import { CompanySearch } from "./company";
 import { searchCompanies } from "./api";
+import ListPortfolio from "./Components/Portfolio/ListPortfolio/ListPortfolio";
 
 function App() {
   const [search, setSearch] = useState<string>("");
+  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
   const [serverError, setServerError] = useState<string>("");
 
@@ -16,9 +18,12 @@ function App() {
     setSearch(e.target.value);
     console.log(e);
   };
-  const onPortfolioCreate = (e: SyntheticEvent) => {
+  const onPortfolioCreate = (e: any) => {
     e.preventDefault();
-    console.log(e);
+    const exist = portfolioValues.find((value) => value === e.target[0].value);
+    if (exist) return;
+    const updatedPortfolio = [...portfolioValues, e.target[0].value];
+    setPortfolioValues(updatedPortfolio);
   };
   const onSearchSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
@@ -38,6 +43,7 @@ function App() {
         handleSearchChange={handleSearchChange}
       />
       {serverError && <h1>{serverError}</h1>}
+      <ListPortfolio portfolioValues={portfolioValues} />
       <CardList
         searchResult={searchResult}
         onPortfolioCreate={onPortfolioCreate}
