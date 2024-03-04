fe1abf5758b1c00398146b958678da7f69f02163
Emre ÖZGÖZ <emreozgoz@gmail.com>
2024-03-04 11:37:58 +0300

    feat: add company dashboard page

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { CompanyProfile } from "../../company";
 import { getCompanyProfile } from "../../api";
+import SideBar from "../../Components/SideBar/SideBar";
+import CompanyDashboard from "../../Components/CompanyDasboard/CompanyDashboard";
+import Tile from "../../Components/Tile/Tile";
 
 interface Props {}
 
@@ -18,7 +21,12 @@ const CompanyPage = (props: Props) => {
   return (
     <>
       {company ? (
-        <div>{company.companyName}</div>
+        <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+          <SideBar />
+          <CompanyDashboard>
+            <Tile title="Company Name" subTitle={company.companyName}></Tile>
+          </CompanyDashboard>
+        </div>
       ) : (
         <div>Company not found</div>
       )}
